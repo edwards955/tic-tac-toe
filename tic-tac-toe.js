8e5a3643d3f468514427bf52f0b82f2404517c3e
edwards955 <getignorantfriday@gmail.com>
2023-08-15 12:18:14 -0400

    Build function used to update game board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,5 @@
+const display = document.querySelectorAll('.cell');
+
 const Player = (name, marker) => {
     const getName = () => name;
     const getMarker = () => marker;
@@ -9,11 +11,24 @@ const gameBoard = (() => {
         ['X', 'O', 'X'],
         ['X', 'X', 'O'],
         ['O', 'O', 'X']
-    ];
+    ]
 
     const markBoard = (marker, x, y) => {
         _board[x][y] = marker;
+        updateBoard();
     }
 
-    return {markBoard};
+    const updateBoard = () => {
+        let position = 0;
+        for (let i = 0; i < _board.length; i++) {
+            for (let j = 0; j < _board[i].length; j++) {
+                display[position].textContent = _board[i][j];
+                position++;
+            }
+        }
+    }
+
+    return {markBoard, updateBoard};
 })();
+
+gameBoard.updateBoard();
