362f500db5418d1e06ac400b19aa5b510f9d068a
edwards955 <getignorantfriday@gmail.com>
2023-08-15 15:49:00 -0400

    Implement GameController and modify GameBoard

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,29 +6,57 @@ const Player = (name, marker) => {
     return {getName, getMarker};
 }
 
-const gameBoard = (() => {
-    const _board = [
+const GameBoard = () => {
+    const board = [
         ['X', 'O', 'X'],
         ['X', 'X', 'O'],
         ['O', 'O', 'X']
     ]
 
     const markBoard = (marker, x, y) => {
-        _board[x][y] = marker;
+        board[x][y] = marker;
         updateBoard();
     }
 
     const updateBoard = () => {
         let position = 0;
-        for (let i = 0; i < _board.length; i++) {
-            for (let j = 0; j < _board[i].length; j++) {
-                display[position].textContent = _board[i][j];
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                display[position].textContent = board[i][j];
                 position++;
             }
         }
     }
 
     return {markBoard, updateBoard};
-})();
+};
 
-gameBoard.updateBoard();
+const GameController = () => {
+    const playerOne = Player('Player 1', 'X');
+    const playerTwo = Player('Player 2', 'O');
+    const board = GameBoard();
+    board.updateBoard();
+    let currentPlayer = playerOne;
+
+    const changePlayerTurn = () => {
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const playRound = (x, y) => {
+        board.markBoard(getCurrentPlayer().getMarker(), x, y);
+        changePlayerTurn();
+    }
+
+    return {getCurrentPlayer, playRound}
+}
+
+let game = GameController();
+display.forEach((cell) => {
+    cell.addEventListener('click', () => {
+        let x = Number(cell.getAttribute('data-x'));
+        let y = Number(cell.getAttribute('data-y'));
+        game.playRound(x, y);
+    })
+});
